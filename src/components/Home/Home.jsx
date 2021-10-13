2878e431b3546977bfafb42f8d27cdc972027833
artginzburg <art.ginzburg@ya.ru>
2021-10-13 19:59:12 +0300

    add commented out backgroundNoise for future

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,21 @@ import './Home.scss';
 export default function Home() {
   return (
     <section className="home">
+      {/* <svg className="svgFilter">
+          <filter id="noise">
+            <feTurbulence id="turbulence">
+              <animate
+                attributeName="baseFrequency"
+                dur="1s"
+                values="0.9 0.9; 0.8 0.8; 0.9 0.9;"
+                repeatCount="indefinite"
+              />
+              <feDisplacementMap in="SourceGraphic" scale="60" />
+            </feTurbulence>
+          </filter>
+        </svg>
+        <div className="backgroundNoise" /> */}
+
       <img src={avatar} className="home__logo" alt="avatar" />
       <p>Я Арт, пишу код</p>
       <a
