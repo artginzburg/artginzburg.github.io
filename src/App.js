f2b1ce5a4dacb0c44fa6414a4a092f03e73ecda9
artginzburg <art.ginzburg@ya.ru>
2021-10-13 18:46:11 +0300

    add my avatar image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import avatar from './images/avatar.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={avatar} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
