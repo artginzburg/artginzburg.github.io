e1e9a12e051dfbad67d2282955150c216a5841db
artginzburg <art.ginzburg@ya.ru>
2021-10-17 15:47:07 +0300

    add portfolio grouping by category

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,34 +4,52 @@ import { projects } from '../../data';
 
 import './Portfolio.scss';
 
+function splitIntoGroups(inputArray) {
+  return inputArray.reduce((resultArray, item) => {
+    const chunkIndex = item.category;
+
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [];
+    }
+
+    resultArray[chunkIndex].push(item);
+
+    return resultArray;
+  }, {});
+}
+
+const categories = splitIntoGroups(projects);
+
 export default function Portfolio() {
   const {
-    // localization,
+    localization,
     state: [lang],
   } = useLanguage();
 
-  // todo: group projects by category
-  // <article>
-  //   <h2 className="portfolio__subtitle">Category</h2>
-  //   <ul>
-  //     project1
-  //     project2
-  //   </ul>
-  // </article>
-
   return (
     <section className="portfolio">
-      {projects.map((project) => {
+      {Object.keys(categories).map((category, categoryIndex) => {
+        const categoryProjects = categories[category];
+
         return (
-          <li>
-            <a href={project.link} target="_blank" rel="noreferrer">
-              {project.title}
-            </a>
-            <p>
-              {project.year}, {project.subtitle}
-            </p>
-            <p>{project.description[lang]}</p>
-          </li>
+          <article key={categoryIndex}>
+            <h2 className="portfolio__subtitle">{localization.portfolio[category]}</h2>
+            <ul>
+              {categoryProjects.map((project, projectIndex) => {
+                return (
+                  <li key={projectIndex}>
+                    <a href={project.link} target="_blank" rel="noreferrer">
+                      {project.title}
+                    </a>
+                    <p>
+                      {project.year}, {project.subtitle}
+                    </p>
+                    <p>{project.description[lang]}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </article>
         );
       })}
     </section>
