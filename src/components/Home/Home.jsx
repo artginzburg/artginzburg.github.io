38316e8283c2f50a5652f696516b91bf5b35ad49
artginzburg <art.ginzburg@ya.ru>
2021-11-02 17:55:24 +0300

    let HiddenWords fade out, put Life and Portfolio on Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,8 @@ import { useTitle } from '../../hooks/useTitle';
 import HiddenWords from '../HiddenWords/HiddenWords';
 
 import './Home.scss';
+import Portfolio from '../Portfolio/Portfolio';
+import Life from '../Life/Life';
 
 const hiddenWords = ['develop', 'software', 'ginzburg'];
 
@@ -17,38 +19,42 @@ export default function Home() {
   useTitle(localization.titles.main);
 
   return (
-    <section className="home">
-      <HiddenWords words={hiddenWords} />
+    <>
+      <section className="home">
+        <HiddenWords words={hiddenWords} />
 
-      <img src={avatar} className="home__logo" alt="avatar" />
-      <p className="home__text">{localization.home_text}</p>
-      <nav className="home__buttons">
-        <a
-          className="home__button"
-          href="https://github.com/artginzburg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="home__button-icon">
-            <FaGithub />
-          </span>
-          <p className="home__button-text">{localization.links.github}</p>
-        </a>
-        <a
-          className="home__button"
-          href="https://t.me/ginzart"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="home__button-icon">
-            <FaTelegramPlane />
-          </span>
-          <p className="home__button-text">{localization.links.telegram}</p>
-        </a>
-      </nav>
-      <Link to="/portfolio" className="home__description">
-        Express.js, MongoDB, React
-      </Link>
-    </section>
+        <img src={avatar} className="home__logo" alt="avatar" />
+        <p className="home__text">{localization.home_text}</p>
+        <nav className="home__buttons">
+          <a
+            className="home__button"
+            href="https://github.com/artginzburg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="home__button-icon">
+              <FaGithub />
+            </span>
+            <p className="home__button-text">{localization.links.github}</p>
+          </a>
+          <a
+            className="home__button"
+            href="https://t.me/ginzart"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="home__button-icon">
+              <FaTelegramPlane />
+            </span>
+            <p className="home__button-text">{localization.links.telegram}</p>
+          </a>
+        </nav>
+        <Link to="/portfolio" className="home__description">
+          Express.js, MongoDB, React
+        </Link>
+      </section>
+      <Life />
+      <Portfolio />
+    </>
   );
 }
