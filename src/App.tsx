c475b7978c7bbc875eaa7a3e79dc7b5456b2fa2a
artginzburg <ginzburg.arthur@gmail.com>
2022-04-03 02:45:52 +0300

    fix routing a bit more

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/portfolio" element={<Portfolio />} />
-            <Route element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
       </LanguageProvider>
