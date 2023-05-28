43abcceb10aed4b7b0e2dae14682bc15553d0524
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-05-28 06:01:20 +0400

    Fix the "test"

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders steam link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/computer games/i);
+  const linkElement = screen.getByText(/yandex.praсtiсum/i);
   expect(linkElement).toBeDefined();
 });
